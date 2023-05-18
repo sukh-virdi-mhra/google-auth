b0a06f452095fcfcbaf0b35fa35cdf9bfdb8142f
Sukh Virdi <sukh.virdi2@mhra.gov.uk>
2023-05-18 20:39:32 +0100

    cleanup frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,7 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
 import "./App.css";
-import Header from "./components/Header";
-import Result from "./components/Result";
-import Search from "./components/Search";
 
 function App() {
-  const [countries, setCountries] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:5000/getCountries").then((response) => {
-      setCountries(response.data);
-    });
-  }, []);
-
-  return (
-    <div className="App">
-      <Header />
-      <Search setCountries={setCountries} />
-      <Result countries={countries} />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
