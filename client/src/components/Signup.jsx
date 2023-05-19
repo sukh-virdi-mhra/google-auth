bd3046e03304754e8b84711b05c17613ae80370f
Sukh Virdi <sukh.virdi2@mhra.gov.uk>
2023-05-19 16:06:55 +0100

    signup component

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import "../styles/styles.css";
 
-function Login() {
+function Signup() {
   const googleAuth = () => {
     window.open(
       `${process.env.REACT_APP_API_URL}/auth/google/callback`,
@@ -10,23 +10,28 @@ function Login() {
   };
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading}>Login Form</h1>
+      <h1 className={styles.heading}>Sign up Form</h1>
       <div className={styles.form_container}>
         <div className={styles.left}>
-          <img className={styles.img} src="./images/login.jpg" alt="login" />
+          <img className={styles.img} src="./images/signup.jpg" alt="signup" />
         </div>
         <div className={styles.right}>
-          <h2 className={styles.from_heading}>Members Login</h2>
+          <h2 className={styles.from_heading}>Create Account</h2>
+          <input type="text" className={styles.input} placeholder="Username" />
           <input type="text" className={styles.input} placeholder="Email" />
-          <input type="text" className={styles.input} placeholder="Password" />
-          <button className={styles.btn}>Login</button>
+          <input
+            type="password"
+            className={styles.input}
+            placeholder="Password"
+          />
+          <button className={styles.btn}>Sign Up</button>
           <p className={styles.text}>or</p>
           <button className={styles.google_btn} onClick={googleAuth}>
             <img src="./images/google.png" alt="google icon" />
-            <span>Sign in with Google</span>
+            <span>Sign up with Google</span>
           </button>
           <p className={styles.text}>
-            New Here? <Link to="/signup">Sign Up</Link>
+            Already Have Account? <Link to="/login">Log In</Link>
           </p>
         </div>
       </div>
@@ -34,4 +39,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Signup;
